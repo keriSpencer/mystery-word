b44509d2581c44c67b916dec806070ffb1af1567
Keri Spencer <kerigspencer@gmail.com>
2017-07-30 17:57:11 -0400

    Guess counter working. Only allows 8 guesses.

--- app.js
+++ app.js
@@ -10,17 +10,28 @@ app.set('view engine', 'mustache')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+let guessCount = 8
+let guessCountMessage = 'You have ' + guessCount + ' guesses remaining.'
 let id = 0
 let loseMessage = 'Sorry, you are out of guesses'
 let guessedLetters = []
 
 app.get('/', (req, res) => {
-  res.render('home', { guessedLetters: guessedLetters })
+  res.render('home', { guessedLetters: guessedLetters, guessCount: guessCount, guessCountMessage: guessCountMessage })
 })
 
 app.post('/', (req, res) => {
   guessedLetters.push({ guess: req.body.guess })
   id++
+  if (guessCount === 1) {
+    guessCountMessage = 'You lose! You are out of guesses'
+  } else if (guessCount === 2) {
+    guessCount--
+    guessCountMessage = 'You have ' + guessCount + ' guess remaining.'
+  } else {
+    guessCount--
+    guessCountMessage = 'You have ' + guessCount + ' guesses remaining.'
+  }
   console.log(id)
   if (id === 8) {
     console.log('Sorry, you are out of guesses')
